80318aab47aa80cd53cd16c579dfb8cc2cde9d8b
Sergio Garcez <garcez.sergio@gmail.com>
2015-03-13 15:16:34 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='redis_counter',
+    version='0.1',
+    author='Smesh',
+    url='https://github.com/Demoncode/redis-counter',
+    description='Time window counters in Python.',
+    long_description=open('README.md').read(),
+    packages=['redis_counter'],
+    zip_safe=True,
+    license='BSD',
+)
