991665198d422a29333764286ecb9eab6a9a4a03
Alex Kelly <alex@sme.sh>
2015-06-11 16:23:25 +0100

    Fix MANIFEST.in to allow setting descrption from README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+import os
+
 from setuptools import setup
 
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(
     name='redis_counter',
@@ -7,7 +14,7 @@ setup(
     author='Smesh',
     url='https://github.com/Demoncode/redis-counter',
     description='Time window counters in Python.',
-    long_description=open('README.md').read(),
+    long_description=(read('README.md')),
     packages=['redis_counter'],
     zip_safe=False,
     license='BSD',
